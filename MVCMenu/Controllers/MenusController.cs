33fee70cccb7a270428c164433f4262fb085c04b
Warren Gerald Polandra <85095564+warrenpolandra@users.noreply.github.com>
2023-03-22 14:48:12 +0700

    Initial Commit

---
+++ MVCMenu/Controllers/MenusController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCMenu.Entities;
+
+namespace MVCMenus.Controllers
+{
+    [Authorize]
+    public class MenusController : Controller
+    {
+        // GET: Menus
+        public ActionResult Index()
+        {
+            List<Menus> m;
+            using (var r = new MenusEntities())
+            {
+                m = r.Menus.ToList();
+            }
+            return View(m);
+        }
+
+        [HttpGet]
+        [ActionName("Create")]
+        public ActionResult Create_Get()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Create")]
+        public ActionResult Create_Post()
+        {
+            var menusmodel = new Menus();
+            TryUpdateModel(menusmodel);
+
+            using (var r = new MenusEntities())
+            {
+                r.Menus.Add(menusmodel);
+                r.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Details(string code)
+        {
+            var menumodel = new Menus();
+            TryUpdateModel(menumodel);
+
+            using (var r = new MenusEntities())
+            {
+                menumodel = r.Menus.FirstOrDefault(x => x.MenuCode == code);
+            }
+
+            return View(menumodel);
+        }
+
+        [HttpGet]
+        [ActionName("Edit")]
+        public ActionResult Edit_Get(string code)
+        {
+            var menumodel = new Menus();
+            TryUpdateModel(menumodel);
+
+            using (var r = new MenusEntities())
+            {
+                menumodel = r.Menus.Where(x => x.MenuCode == code).FirstOrDefault();
+            }
+
+            return View(menumodel);
+        }
+
+        [HttpPost]
+        [ActionName("Edit")]
+        public ActionResult Edit_Post(string code)
+        {
+            var menumodel = new Menus();
+            TryUpdateModel(menumodel);
+
+            using (var r = new MenusEntities())
+            {
+                var m = r.Menus.Where(x => x.MenuCode == code).FirstOrDefault();
+                TryUpdateModel(m);
+                r.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [ActionName("Delete")]
+        public ActionResult Delete_Get(string code)
+        {
+            var menumodel = new Menus();
+            TryUpdateModel(menumodel);
+
+            using (var r = new MenusEntities())
+            {
+                menumodel = r.Menus.FirstOrDefault(x => x.MenuCode == code);
+            }
+
+            return View(menumodel);
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult Delete_Post(string code)
+        {
+            var menumodel = new Menus();
+            TryUpdateModel(menumodel);
+
+            using (var r = new MenusEntities())
+            {
+                var m = r.Menus.Remove(r.Menus.FirstOrDefault(x => x.MenuCode == code));
+                TryUpdateModel(m);
+                r.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MVCMenu/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCMenu.MvcApplication" Language="C#" %>
